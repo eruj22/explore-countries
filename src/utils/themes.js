8ea14f3e63e951ba34237330fa266fcd9526d61b
eruj22 <prci.prnaver2@gmail.com>
2021-10-31 14:35:09 +0100

    added button scroll to top

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -2,10 +2,14 @@ export const lightTheme = {
   body: "hsl(0, 0%, 98%)",
   text: "hsl(200, 15%, 8%)",
   elements: "hsl(0, 0%, 100%)",
+  goTopText: "hsl(20, 10%, 90%)",
+  goTopBg: "hsl(207, 26%, 17%)",
 }
 
 export const darkTheme = {
   body: "hsl(207, 26%, 17%)",
   text: "hsl(20, 10%, 90%)",
   elements: "hsl(209, 23%, 22%)",
+  goTopText: "hsl(200, 15%, 8%)",
+  goTopBg: "hsl(0, 0%, 98%)",
 }
